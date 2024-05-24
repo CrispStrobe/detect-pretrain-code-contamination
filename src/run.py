a1f61908abba039ab265d78d26035564eb0ccb47
CrispStrobe <154636388+CrispStrobe@users.noreply.github.com>
2024-05-24 22:24:35 +0200

    Update run.py

--- src/run.py
+++ src/run.py
@@ -22,7 +22,7 @@ from eval import (
     process_truthful_qa,
     process_gsm8k,
     process_arc
-    process_winongrade,
+    process_winogrande,
 )
 from options import Options
 from utils import evaluate_model
